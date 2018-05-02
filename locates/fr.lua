30438db496d483d35a3ae8c22ce5fa8e9ccdd3ae
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2018-05-02 12:18:58 +0200

    FR translation2

---
+++ locates/fr.lua
@@ -0,0 +1,14 @@
+Locales['fr'] = {
+	-- ooc chat
+	['ooc_help'] = 'Écrit un message hors du personnage',
+	['ooc_prefix'] = 'HRP | %s',
+	['twt_help'] = 'Envoie un tweet',
+	['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+	['me_help'] = 'action personnelle',
+	['me_prefix'] = 'me | %s',
+	['news_help'] = 'Annonce une information (ne pas en abuser)',
+	['news_prefix'] = 'Info | %s',
+	['ooc_argument_name'] = 'message',
+	['ooc_argument_help'] = 'le message',
+	['ooc_unknown_command'] = '^1%s^0 n\'est pas une commande valide!',
+}
